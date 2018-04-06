46bf54757c5d9f3cb91ff4cc981b9293849c0a10
EricKeiser <eric.keiser1@gmail.com>
2018-04-05 23:35:02 -0400

    deleted vegas information files

--- vegas.js
+++
@@ -1,7 +0,0 @@
-fetch('https://draftmanager.com/mlb/no-vig-vegas-lines/.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-  });
