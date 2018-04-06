c770aecd206c1897f41bef9deaca354f57f23158
EricKeiser <eric.keiser1@gmail.com>
2018-04-05 23:46:38 -0400

    added Fanduel batters

---
+++ batters/index.js
@@ -0,0 +1,73 @@
+var spreadsheet = "https://docs.google.com/spreadsheets/d/1vZcl6_FiTD36-OM_Ao9UIk4u2uI8BNEdHpfPlEuOFfQ/edit#gid=318804173";
+
+$(document).ready(function(){
+  $('#pitchers').sheetrock({
+    url: spreadsheet, 
+    query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X",
+    callback: function draw(error, options, response) {
+if (error) {
+console.log('fail');
+}
+else{
+console.log(response.html);
+}
+}
+});
+setTimeout(function(){
+
+$("table").tablesorter({
+theme : "bootstrap",
+
+widthFixed: true,
+
+// widget code contained in the jquery.tablesorter.widgets.js file
+// use the zebra stripe widget if you plan on hiding any rows (filter widget)
+// the uitheme widget is NOT REQUIRED!
+widgets : [ "filter", "columns", "zebra" ],
+
+widgetOptions : {
+  // using the default zebra striping class name, so it actually isn't included in the theme variable above
+  // this is ONLY needed for bootstrap theming if you are using the filter widget, because rows are hidden
+  zebra : ["even", "odd"],
+
+  // class names added to columns when sorted
+  columns: [ "primary", "secondary", "tertiary" ],
+
+  // reset filters button
+  filter_reset : ".reset",
+
+  // extra css class name (string or array) added to the filter element (input or select)
+  filter_cssFilter: [
+    'form-control',
+    'form-control',
+    'form-control', 
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control',
+    'form-control'
+    
+  ]
+
+}
+});
+}, 500); 
+$("table").trigger("update");
+console.log()
+});
